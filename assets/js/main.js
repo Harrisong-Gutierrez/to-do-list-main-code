1db045a443e82c373febfb0f4dfb406985043383
Harrisong <harrygutie7@gmail.com>
2023-07-19 21:33:50 -0600

    9:33

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 (function () {
   const module = {
     tasks: [],
-    taskIdCounter: 0, // Contador para generar identificadores únicos
+    taskIdCounter: 0,
+    bodyElement: null,
 
     cacheDom: function () {
       this.formContainer = document.getElementById('form-container');
@@ -16,84 +17,63 @@
       this.mainBox = document.querySelector('main');
       this.headerBox = document.getElementById('header-box');
       this.taskCounter = document.getElementById('couter');
-      this.body = document.body;
+      this.bodyElement = document.querySelector('body');
     },
 
     main: function () {
       this.cacheDom();
       this.loadTasksFromLocalStorage();
       this.renderList();
-      this.bindingEvents();
+      this.bindEvents();
     },
 
     renderList: function () {
-      this.cacheDom();
-
       const list = document.getElementById('list-to-do');
 
-      if (!list) {
-        const newList = document.createElement('ul');
-        newList.id = 'list-to-do';
-        newList.className = 'list-group';
-        this.listContainer.appendChild(newList);
-      }
+      const newList = list
+        ? null
+        : ((newList = document.createElement('ul')),
+          (newList.id = 'list-to-do'),
+          (newList.className = 'list-group'),
+          this.listContainer.appendChild(newList));
 
-      const tasks = this.tasks;
       const isDarkMode = this.checkboxToggle.checked;
+      const listItems = list ? list.querySelectorAll('li') : [];
+      listItems.forEach((listItem) => listItem.remove());
 
-      const listItems = list.querySelectorAll('li');
-      listItems.forEach(function (listItem) {
-        listItem.remove();
-      });
-
-      tasks.forEach(function (task) {
-        if (!task) {
-          return;
-        }
-
+      this.tasks.forEach((task) => {
         const listItem = document.createElement('li');
-        listItem.id = 'list-item-' + task.id;
-        listItem.className = 'BodyPrincipal-list-custom d-flex justify-content-between align-items-center';
-
-        if (isDarkMode) {
-          listItem.classList.add('BodyPrincipal-list-custom-dark');
-        } else {
-          listItem.classList.add('BodyPrincipal-list-custom-light');
-        }
+        listItem.id = `list-item-${task.id}`;
+        listItem.className = `BodyPrincipal-list-custom d-flex justify-content-between align-items-center ${
+          isDarkMode ? 'BodyPrincipal-list-custom-dark' : 'BodyPrincipal-list-custom-light'
+        }`;
 
         const checkboxDiv = document.createElement('div');
         checkboxDiv.className = 'BodyPrincipal-checkbox';
         const checkbox = document.createElement('div');
         checkbox.type = 'radio';
-        checkbox.className = '<fa-sharp fa-solid fa-circle-check BodyPrincipal-radio-task';
-        checkbox.id = 'task-' + task.id;
+        checkbox.className = `<fa-sharp fa-solid fa-circle-check BodyPrincipal-radio-task`;
+        checkbox.id = `task-${task.id}`;
 
-        checkbox.addEventListener('click', function () {
+        checkbox.addEventListener('click', () => {
           checkbox.checked = !checkbox.checked;
-          if (checkbox.checked) {
-            label.style.textDecoration = 'line-through';
-            task.completed = true;
-          } else {
-            label.style.textDecoration = 'none';
-            task.completed = false;
-          }
-          module.saveTasksToLocalStorage();
+          const label = listItem.querySelector('label');
+          label.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
+          task.completed = checkbox.checked;
+          this.saveTasksToLocalStorage();
         });
+
         checkboxDiv.appendChild(checkbox);
 
         const label = document.createElement('label');
-        label.htmlFor = 'task-' + task.id;
+        label.htmlFor = `task-${task.id}`;
         label.textContent = task.text;
         label.className = 'BodyPrincipal-label-task';
-        
 
-        if (task.completed) {
-          checkbox.checked = true;
-          label.style.textDecoration = 'line-through';
-        }
+        checkbox.checked = task.completed;
+        label.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
 
         checkboxDiv.appendChild(label);
-
         listItem.appendChild(checkboxDiv);
 
         const buttonsDiv = document.createElement('div');
@@ -112,31 +92,23 @@
         listItem.appendChild(buttonsDiv);
         list.appendChild(listItem);
 
-        editButton.addEventListener('click', function () {
-          module.editTask(task, task.id);
-        });
-
-        deleteButton.addEventListener('click', function () {
-          module.deleteTask(task.id);
-        });
+        editButton.addEventListener('click', () => this.editTask(task, task.id));
+        deleteButton.addEventListener('click', () => this.deleteTask(task.id));
       });
 
-      const taskCounter = tasks.length;
-      this.taskCounter.textContent = taskCounter;
-
-      module.saveTasksToLocalStorage();
+      this.taskCounter.textContent = this.tasks.length;
+      this.saveTasksToLocalStorage();
     },
 
     addTaskToList: function (task) {
       const newTask = {
         id: this.taskIdCounter,
         text: task,
-        completed: false
+        completed: false,
       };
 
       this.tasks.push(newTask);
       this.taskIdCounter++;
-
       this.renderList();
     },
 
@@ -149,146 +121,113 @@
       this.tasks = savedTasks ? JSON.parse(savedTasks) : [];
     },
 
-  
-  editTask: function (task, taskId) {
-  const listItem = document.getElementById('list-item-' + taskId);
-  const label = listItem.querySelector('label');
+    editTask: function (task, taskId) {
+      const listItem = document.getElementById(`list-item-${taskId}`);
+      const label = listItem.querySelector('label');
 
-  const existingInput = listItem.querySelector('.BodyPrincipal-input-edit-task');
-  const existingButton = listItem.querySelector('.BodyPrincipal-button-update-task');
+      const existingInput = listItem.querySelector('.BodyPrincipal-input-edit-task');
+      const existingButton = listItem.querySelector('.BodyPrincipal-button-update-task');
 
-  if (existingInput && existingButton) {
-    existingInput.remove();
-    existingButton.remove();
-  }
+      existingInput?.remove();
+      existingButton?.remove();
 
-  const inputEdit = document.createElement('input');
-  inputEdit.type = 'text';
-  inputEdit.value = task.text;
-  inputEdit.className = 'BodyPrincipal-input-edit-task';
+      const inputEdit = document.createElement('input');
+      inputEdit.type = 'text';
+      inputEdit.value = task.text;
+      inputEdit.className = 'BodyPrincipal-input-edit-task';
 
-  const updateButton = document.createElement('button');
-  updateButton.className = 'BodyPrincipal-button-update-task';
-  updateButton.textContent = 'Update';
+      const updateButton = document.createElement('button');
+      updateButton.className = 'BodyPrincipal-button-update-task';
+      updateButton.textContent = 'Update';
 
-  listItem.insertBefore(inputEdit, label.nextSibling);
-  listItem.insertBefore(updateButton, label.nextSibling);
+      listItem.insertBefore(inputEdit, label.nextSibling);
+      listItem.insertBefore(updateButton, label.nextSibling);
 
-  label.style.display = 'none'; // Ocultamos la etiqueta durante la edición
-  listItem.querySelector('.BodyPrincipal-buttons').style.display = 'none'; // Ocultamos los botones durante la edición
+      label.style.display = 'none';
+      listItem.querySelector('.BodyPrincipal-buttons').style.display = 'none';
 
-  updateButton.addEventListener('click', function () {
-    const updatedTask = inputEdit.value.trim();
-    if (updatedTask !== '') {
-      task.text = updatedTask;
-      module.renderList();
-    }
-    listItem.removeChild(inputEdit);
-    listItem.removeChild(updateButton);
-    label.style.display = ''; // Mostramos nuevamente la etiqueta después de la edición
-    listItem.querySelector('.BodyPrincipal-buttons').style.display = ''; // Mostramos nuevamente los botones después de la edición
-    label.textContent = task.text; // Actualizamos el valor de la etiqueta con el valor editado
-  });
+      updateButton.addEventListener('click', () => {
+        const updatedTask = inputEdit.value.trim();
+        updatedTask !== '' && ((task.text = updatedTask), this.renderList());
+        inputEdit.remove();
+        updateButton.remove();
+        label.style.display = '';
+        listItem.querySelector('.BodyPrincipal-buttons').style.display = '';
+        label.textContent = task.text;
+      });
 
-  inputEdit.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-      const updatedTask = inputEdit.value.trim();
-      if (updatedTask !== '') {
-        task.text = updatedTask;
-        module.renderList();
-      }
-      listItem.removeChild(inputEdit);
-      listItem.removeChild(updateButton);
-      label.style.display = ''; // Mostramos nuevamente la etiqueta después de la edición
-      listItem.querySelector('.BodyPrincipal-buttons').style.display = ''; // Mostramos nuevamente los botones después de la edición
-      label.textContent = task.text; // Actualizamos el valor de la etiqueta con el valor editado
-    }
-  });
-},
-
-  
-
-    deleteTask: function (taskId) {
-      const taskIndex = this.tasks.findIndex(task => task.id === taskId);
-      if (taskIndex !== -1) {
-        this.tasks.splice(taskIndex, 1);
-        this.renderList();
-      }
+      inputEdit.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+          const updatedTask = inputEdit.value.trim();
+          updatedTask !== '' && ((task.text = updatedTask), this.renderList());
+          inputEdit.remove();
+          updateButton.remove();
+          label.style.display = '';
+          listItem.querySelector('.BodyPrincipal-buttons').style.display = '';
+          label.textContent = task.text;
+        }
+      });
     },
 
-    bindingEvents: function () {
-      window.addEventListener('DOMContentLoaded', function () {
+    deleteTask: function (taskId) {
+      const taskIndex = this.tasks.findIndex((task) => task.id === taskId);
+      taskIndex !== -1 && (this.tasks.splice(taskIndex, 1), this.renderList());
+    },
+
+    bindEvents: function () {
+      window.addEventListener('DOMContentLoaded', () => {
         const savedDarkMode = localStorage.getItem('darkMode');
-
-        if (savedDarkMode === 'true') {
-          module.checkboxToggle.checked = true;
-          enableDarkMode();
-        } else {
-          module.checkboxToggle.checked = false;
-          disableDarkMode();
-        }
+        this.checkboxToggle.checked = savedDarkMode === 'true';
+        this.checkboxToggle.checked ? this.enableDarkMode() : this.disableDarkMode();
       });
 
-      const handleAddButton = function (event) {
+      this.addButton.addEventListener('click', (event) => {
         event.preventDefault();
-        const inputValue = module.input.value.trim();
-
-        if (inputValue !== '') {
-          module.addTaskToList(inputValue);
-          module.input.value = '';
-        }
-      };
-
-      this.addButton.addEventListener('click', handleAddButton);
-      this.input.addEventListener('keydown', function (event) {
-        if (event.key === 'Enter') {
-          handleAddButton(event);
-        }
+        const inputValue = this.input.value.trim();
+        inputValue !== '' && (this.addTaskToList(inputValue), (this.input.value = ''));
       });
 
-      this.toggle.addEventListener('change', function (event) {
+      this.input.addEventListener('keydown', (event) => {
+        event.key === 'Enter' && this.addButton.click();
+      });
+
+      this.toggle.addEventListener('change', (event) => {
         const checked = event.target.checked;
-
-        if (checked) {
-          enableDarkMode();
-        } else {
-          disableDarkMode();
-        }
-
+        checked ? this.enableDarkMode() : this.disableDarkMode();
         localStorage.setItem('darkMode', checked.toString());
       });
-    }
+    },
+
+    enableDarkMode: function () {
+      this.bodyElement.classList.add('BodyPrincipalDark');
+      this.mainBox.classList.add('BodyPrincipalDark-dark-main-box');
+      this.headerBox.classList.add('BodyPrincipalDark-header-box');
+
+      const taskListItems = this.listContainer.querySelectorAll('li');
+      taskListItems.forEach((item) => {
+        item.classList.remove('BodyPrincipal-list-custom-light');
+        item.classList.add('BodyPrincipal-list-custom-dark');
+      });
+
+      this.labelToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+      this.labelToggle.style.color = 'orange';
+    },
+
+    disableDarkMode: function () {
+      this.bodyElement.classList.remove('BodyPrincipalDark');
+      this.mainBox.classList.remove('BodyPrincipalDark-dark-main-box');
+      this.headerBox.classList.remove('BodyPrincipalDark-header-box');
+
+      const taskListItems = this.listContainer.querySelectorAll('li');
+      taskListItems.forEach((item) => {
+        item.classList.remove('BodyPrincipal-list-custom-dark');
+        item.classList.add('BodyPrincipal-list-custom-light');
+      });
+
+      this.labelToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+      this.labelToggle.style.color = 'purple';
+    },
   };
-
-  function enableDarkMode() {
-    module.body.classList.add('BodyPrincipalDark');
-    module.mainBox.classList.add('BodyPrincipalDark-dark-main-box');
-    module.headerBox.classList.add('BodyPrincipalDark-header-box');
-
-    const taskListItems = module.listContainer.querySelectorAll('li');
-    taskListItems.forEach(function (item) {
-      item.classList.remove('BodyPrincipal-list-custom-light');
-      item.classList.add('BodyPrincipal-list-custom-dark');
-    });
-
-    module.labelToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
-    module.labelToggle.style.color = 'orange';
-  }
-
-  function disableDarkMode() {
-    module.body.classList.remove('BodyPrincipalDark');
-    module.mainBox.classList.remove('BodyPrincipalDark-dark-main-box');
-    module.headerBox.classList.remove('BodyPrincipalDark-header-box');
-
-    const taskListItems = module.listContainer.querySelectorAll('li');
-    taskListItems.forEach(function (item) {
-      item.classList.remove('BodyPrincipal-list-custom-dark');
-      item.classList.add('BodyPrincipal-list-custom-light');
-    });
-
-    module.labelToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
-    module.labelToggle.style.color = 'purple';
-  }
 
   module.main();
 })();
